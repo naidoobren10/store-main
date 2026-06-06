28f47b8049dc5a0e0b43ebc6d5282c5c44d8d08b
Brendan naidoo <brendannaidoo@Brendans-MacBook-Pro.local>
2026-06-06 13:00:14 +0200

    fix: Overriding findAll method with EntityGraph as Customer repository loads Many orders lazily, which could cause an N+1 problems, which may lead to performance bottlenecks when retrieveing all customers with their associted orders. Applied similar fix to retrieve all Orders, which load a customer per order lazily. This could be problematic where there are latency issues between the application and database

--- src/main/java/com/example/store/repository/OrderRepository.java
+++ src/main/java/com/example/store/repository/OrderRepository.java
@@ -2,6 +2,17 @@ package com.example.store.repository;
 
 import com.example.store.entity.Order;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = "customer")
+    List<Order> findAll();
+
+
+}
