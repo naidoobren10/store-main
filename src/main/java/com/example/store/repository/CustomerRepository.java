b3dae646cb31cb10d8d1c708cdd8827592a729dd
naidoobren10 <117037797+naidoobren10@users.noreply.github.com>
2026-06-06 13:04:42 +0200

    Feature/interview tasks (#4)
    * fix: Refactoring the OrderController, to move business logic out of the controller into a service. Unit tests refactored to accomodate changes
    
    * refactor: Refactored the Order entity to replace @Data annotations with normal @getter and @setter to prevent automatic generation of hash, toString, isEquals methods. Created a seperate OrderRequestDTO, with additional validations for fields. Validations included on the controller and at service level. Creates custom exceptions for handling customers and orders. Created a Global Rest Controller advice to handle exceptions globally across all rest controllers
    
    * fix: discarding irrelevant class
    
    * refactor: Refactored the Order entity to replace @Data annotations with normal @getter and @setter to prevent automatic generation of hash, toString, isEquals methods. Created a seperate OrderRequestDTO, with additional validations for fields. Validations included on the controller and at service level. Creates custom exceptions for handling customers and orders. Created a Global Rest Controller advice to handle exceptions globally across all rest controllers
    
    * refactor: refactoring the Customer domain - controller, entity and unit tests. Moved business logic into a seperate service layer. Additional DTO created for request. Validation included for customer domain
    
    * feat: Extended the OrderController to be able to retrieve an order by id. Extended the Customer controller to be able to search for a Name using a queryString which could be a substring of the name
    
    * feat: Extended the OrderController to be able to retrieve an order by id. Extended the Customer controller to be able to search for a Name using a queryString which could be a substring of the name
    
    * build: add validation and testcontainer dependencies
    
    * fix: Overriding findAll method with EntityGraph as Customer repository loads Many orders lazily, which could cause an N+1 problems, which may lead to performance bottlenecks when retrieveing all customers with their associted orders. Applied similar fix to retrieve all Orders, which load a customer per order lazily. This could be problematic where there are latency issues between the application and database
    
    ---------
    
    Co-authored-by: Brendan naidoo <brendannaidoo@Brendans-MacBook-Pro.local>

--- src/main/java/com/example/store/repository/CustomerRepository.java
+++ src/main/java/com/example/store/repository/CustomerRepository.java
@@ -2,14 +2,22 @@ package com.example.store.repository;
 
 import com.example.store.entity.Customer;
 
+import com.example.store.entity.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = "orders")
+    List<Customer> findAll();
 
     @Query(value = """
     select *
